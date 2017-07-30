d7995d29d2d73db4e463a1829c618f127ff33b0a
Cameron <mooreheadc@hotmail.com>
2017-07-30 14:31:49 -0700

    mongodb un/pw

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 // use es6 promises
 mongoose.Promise = global.Promise
-process.env.MONGODB_URI = "mongodb://moorehcs:gtr55p90@ds129053.mlab.com:29053/todo-api1121"
+process.env.MONGODB_URI = "mongodb://admin:password@ds129053.mlab.com:29053/todo-api1121"
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp")
 
 module.exports = { mongoose }
