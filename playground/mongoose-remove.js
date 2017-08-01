62622451b098c9a43ad0949e9210ff15a9c70e12
Cameron <mooreheadc@hotmail.com>
2017-07-31 20:23:35 -0700

    DELETE /todos/:id added with tests

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const { ObjectID } = require("mongodb")
+const { mongoose } = require("./../server/db/mongoose")
+const { Todo } = require("./../server/models/todo")
+const { User } = require("./../server/models/user")
+
+// Todo.remove({})
+//   .then(result =>  console.log(result))
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove("597fec253d357c2c13739374")
+  .then(todo => console.log(todo))
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -109,3 +109,41 @@ describe("GET /todos/:id", () => {
       .end(done)
   })
 })
+
+describe("DELETE /todos/:id", () => {
+  it("should delete todo with given id", done => {
+    const hexId = todos[1]._id.toHexString()
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[1].text)
+      })
+      .end((err, res) => {
+        if (err) return done(err)
+
+        Todo.findById(hexId)
+          .then(todo => {
+            expect(todo).toNotExist()
+            done()
+          })
+          .catch(err => done(err))
+      })
+  })
+
+  it("should return 404 if todo not found", done => {
+    const id = new ObjectID()
+    request(app)
+      .delete(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it("should return 404 for not-object ids", done => {
+    request(app)
+      .delete("todos/123")
+      .expect(404)
+      .end(done)
+  })
+})
