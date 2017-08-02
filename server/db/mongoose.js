65d9bf7b07a6ea8b6db7f98d7d667998d6b8cd2e
Cameron <mooreheadc@hotmail.com>
2017-08-01 18:54:04 -0700

    Added test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose")
 
 // use es6 promises
 mongoose.Promise = global.Promise
-process.env.MONGODB_URI = "mongodb://admin:password@ds129053.mlab.com:29053/todo-api1121"
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp")
+mongoose.connect(process.env.MONGODB_URI)
 
 module.exports = { mongoose }
