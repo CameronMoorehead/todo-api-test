46baba1337556da237fcf8f6867569ca55552263
Cameron <mooreheadc@hotmail.com>
2017-08-19 22:32:16 -0700

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.post("/todos", (req, res) => {
 
   todo.save()
     .then(doc => res.send(doc))
-    .catch(err => res.status(400).send(err))
+    .catch(err => res.status(400).send())
 })
 
 app.get("/todos", (req, res) => {
@@ -98,6 +98,23 @@ app.post("/users", (req, res) => {
 
 app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user)
+})
+
+// POST /users/login { email, password }
+
+app.post("/users/login", (req, res) => {
+  const body = _.pick(req.body, ["email", "password"])
+
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      return user.generateAuthToken()
+        .then(token => {
+          res.header("x-auth", token).send(user)
+        })
+    })
+    .catch(err => {
+      res.status(400).send()
+    })
 })
 
 
